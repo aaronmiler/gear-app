0779b90775a6fb632217fd4844f1612c593dcc83
Aaron Miler <aaron@aaronmiler.com>
2013-05-26 22:20:20 -0700

    WIP

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -50,7 +50,6 @@ class FrontendController < ApplicationController
 			time: @date-1.day
 		)
 		@gear = OpenStruct.new(:sunglasses => false,:goggles => false,:rain => false,:wind => false,)
-		render json: @forecast.hourly.data.select{ |x| x['time'] == 1369562400 }.first['visibility']
 	end
 	def multiple_results
 		@locations = session[:results]
