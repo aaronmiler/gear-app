519b784f5b5b20fe2d0fc69e258d1694f4deb088
Aaron Miler <aaron@aaronmiler.com>
2013-05-27 08:33:10 -0700

    WIP

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -53,7 +53,7 @@ class FrontendController < ApplicationController
 
 		params[:times].each do |array, t|
 			@hourLoop = t["ampm"] == 'PM' ? t["h"].to_i + 12 : t["h"].to_i
-			@timeLoop =  @date.change({:hour => @hourLoop, :min => 00, :sec => 00,:usec => 00}).to_i + 1
+			@timeLoop =  @date.change({:hour => @hourLoop, :min => 00, :sec => 00,:usec => 00})
 			@todayLoop = @forecast.hourly.data.select{ |x| x["time"] == @timeLoop }
 			render json: {time: @timeLoop , forecast: @forecast}
 		end
