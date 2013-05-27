1c28e715ba591bee30f69659eec8b3049df0f3d5
Aaron Miler <aaron@aaronmiler.com>
2013-05-26 18:59:47 -0700

    Fixing api key?

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -2,7 +2,7 @@ class FrontendController < ApplicationController
 	require 'forecast_io'
 	require 'ostruct'
 	require 'tzinfo'
-	Forecast::IO.api_key = ENV['FORECAST_KEY']
+	Forecast::IO.api_key = ENV["FORECAST_KEY"]
 
 
 	def index
