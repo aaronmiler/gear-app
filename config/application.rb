77eab4c31d3c577c213fba3df1bee359da120044
Aaron Miler <aaron@aaronmiler.com>
2013-05-29 22:19:57 -0700

    Adding timezone check?

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module WeatherOrNot
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.active_record.default_timezone = :local
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
